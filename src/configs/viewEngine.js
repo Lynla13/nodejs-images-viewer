9681975301ebca22dd2a4b765781b9131417f196
Lynla13 <lynla1318@gmail.com>
2022-12-22 10:18:51 +0700

    statics file

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,7 +1,8 @@
-import { Express } from "express";
+import express from "express";
 
 //Cau hinh express
 const configViewEngine = (app) => {
+    app.use(express.static('./src/public'))
     app.set("view engine", "ejs");
     app.set("views", "./src/views")
 }
